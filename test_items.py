750ff5329723855e8e598ba5e784d6dfe5450cb5
Alexey Yumanov <inactive0073@gmail.com>
2024-08-25 11:51:54 +0400

    Fixed name method in TestPage

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
 class TestPage:
-    def test_page(self, browser):
+    def test_guest_should_see_cart_button(self, browser):
         browser.implicitly_wait(10)
         browser.get(link)
         btn = browser.find_elements(By.CSS_SELECTOR, 'button.btn-add-to-basket')
@@ -14,3 +14,4 @@ class TestPage:
             assert len(btn) == 1, 'Элемент не найден'
         if len(btn) > 1:
             assert len(btn) == 1, 'Найдено более одного элемента'
+
